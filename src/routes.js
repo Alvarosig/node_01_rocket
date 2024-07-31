da05f8bcdb4f150ee9618b8e65729d2cd5638e15
Alvaro <alvaro_s.g@hotmail.com>
2024-07-30 23:56:39 -0300

    feat: created update and delete methos in db, created put and delete endpoints and regex for path params

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,69 @@
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
+import { randomUUID } from "node:crypto"
+import { Database } from "./database.js"
+import { buildRoutePath } from "./utils/buid-route-path.js"
 
-const database= new Database()
+const database = new Database()
 
 export const routes = [
   {
-    method: 'GET',
-    path: '/task',
+    method: "GET",
+    path: buildRoutePath("/task"),
     handler: (req, res) => {
-      const tasks = database.select('tasks')
+      const tasks = database.select("tasks")
 
       return res.end(JSON.stringify(tasks))
-    }
+    },
   },
   {
-    method: 'POST',
-    path: '/task',
+    method: "POST",
+    path: buildRoutePath("/task"),
     handler: (req, res) => {
       const { title, description } = req.body
-      
+
       const task = {
         id: randomUUID(),
         title,
         description,
         completed_at: null,
         created_at: new Date(),
-        updated_at: null
+        updated_at: null,
       }
 
-      database.insert('tasks', task)
+      database.insert("tasks", task)
 
-      return res.writeHead(201).end('Task created')
-    }
-  }
+      return res.writeHead(201).end("Task created")
+    },
+  },
+  {
+    //FIX: created_at doesn't show up in the body after update
+    method: "PUT",
+    path: buildRoutePath("/task/:taskId"),
+    handler: (req, res) => {
+      const { taskId } = req.params
+      const { title, description, created_at } = req.body
+
+      console.log(req.body)
+
+      database.update("tasks", taskId, {
+        title,
+        description,
+        created_at: created_at,
+        updated_at: new Date(),
+        completed_at: null,
+      })
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/task/:taskId"),
+    handler: (req, res) => {
+      const { taskId } = req.params
+
+      database.delete("tasks", taskId)
+
+      return res.writeHead(204).end()
+    },
+  },
 ]
