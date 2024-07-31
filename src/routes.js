f0ca374296e3ec12a0bfc7a70b4adf479785c8b3
Alvaro <alvaro_s.g@hotmail.com>
2024-07-31 13:54:08 -0300

    fix: updated, fixed and implemented validation for the put method and fixed patch metod, crud complete

--- src/routes.js
+++ src/routes.js
@@ -20,9 +20,14 @@ export const routes = [
     handler: (req, res) => {
       const { title, description } = req.body
 
+      // Check Validation
+      if(!title.trim() || !description.trim()) {
+        return res.writeHead(400).end("Title and description are required")
+      }
+
       const task = {
         id: randomUUID(),
-        title,
+        title: title,
         description,
         completed_at: null,
         created_at: new Date(),
@@ -35,19 +40,26 @@ export const routes = [
     },
   },
   {
-    //FIX: created_at doesn't show up in the body after update
     method: "PUT",
     path: buildRoutePath("/task/:taskId"),
     handler: (req, res) => {
       const { taskId } = req.params
-      const { title, description, created_at } = req.body
+      const { title, description } = req.body
 
-      console.log(req.body)
+      const taskToUpdate =  database.select("tasks").find(task => task.id === taskId)
+
+      if(!taskToUpdate) {
+        return res.writeHead(400).end("Task ID not found")
+      }
+      
+      if(!title.trim() || !description.trim()) {
+        return res.writeHead(400).end("Title and description are required")
+      }
 
       database.update("tasks", taskId, {
-        title,
-        description,
-        created_at: created_at,
+        title: title || taskToUpdate.title,
+        description: description || taskToUpdate.description,
+        created_at: taskToUpdate.created_at,
         updated_at: new Date(),
         completed_at: null,
       })
@@ -62,6 +74,22 @@ export const routes = [
       const { taskId } = req.params
 
       database.delete("tasks", taskId)
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: "PATCH",
+    path: buildRoutePath("/task/:taskId/complete"),
+    handler: (req, res) => {
+      const { taskId } = req.params
+
+      const taskToUpdate =  database.select("tasks").find(task => task.id === taskId)
+
+      database.update("tasks", taskId, {
+        ...taskToUpdate,
+        completed_at: new Date(),
+      })
 
       return res.writeHead(204).end()
     },
